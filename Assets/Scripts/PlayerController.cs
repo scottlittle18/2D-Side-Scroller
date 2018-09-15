e3acba2af2daaa36411dd4f05b73a5e3eeaf22dd
Scott Little <scott.little@loop.colum.edu>
2018-09-15 03:15:58 -0500

    Import Character Sprite Sheet
    -Imported Player SpriteSheet and Created Idle Animation.
    -Started Scripting the PlayerController Script
    -Sliced TileMap and adjusted the bottom four rows sprite sizes in the sprite editor

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    public float moveSpeed, jumpHeight;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, jumpHeight);
+        }
+		
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, 0);
+        }
+
+        if (Input.GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, 0);
+	}
+}
