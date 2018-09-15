ba71e6f2a8256496047d85026e722f8fc16c4639
Scott Little <scott.little@loop.colum.edu>
2018-09-15 18:26:11 -0500

    Add PlayerController and GroundCheck
    -Fixed and updated the PlayerController Script
    -Created a Prefab for the Samurai Character for the Player
    -added GroundCheck child to Player in preperation for the Double Jump Upgrade
    -Added Collision to Player Sprite
    -Added Collision to the GrassGround

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,28 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
-    public float moveSpeed, jumpHeight;
+    public float moveSpeed, jumpHeight, groundCheckRadius;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    public Transform groundCheck;
+    public LayerMask whatIsGround;
+    public bool grounded;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
             GetComponent<Rigidbody2D>().velocity = new Vector2(0, jumpHeight);
         }
-		
+
         if (Input.GetKeyDown(KeyCode.D))
         {
             GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, 0);
         }
 
-        if (Input.GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, 0);
-	}
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, 0);
+        }
+    }
 }
