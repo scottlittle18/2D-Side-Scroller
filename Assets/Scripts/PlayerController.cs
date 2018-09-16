2e6cec06d8594be37c959ec48ebfa8df9923518a
Scott Little <scott.little@loop.colum.edu>
2018-09-15 19:25:02 -0500

    Add DoubleJump and Fix Controller
    -fixed problems with the player controller that caused the controller to only move once and had to be released and pressed again
    -added double jump to the player controller
    -added ground check child to character sprite and added related variables and code to Player Controller Script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,9 +7,13 @@ public class PlayerController : MonoBehaviour
 
     public float moveSpeed, jumpHeight, groundCheckRadius;
 
+    //Ground variables
     public Transform groundCheck;
     public LayerMask whatIsGround;
-    public bool grounded;
+    private bool grounded;
+
+    //creates the doubleJumped variable
+    private bool doubleJumped;
 
     // Use this for initialization
     void Start()
@@ -17,6 +21,7 @@ public class PlayerController : MonoBehaviour
 
     }
 
+    //Ground Check
     void FixedUpdate()
     {
         grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
@@ -25,18 +30,31 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //resets the doubleJump variable when the player touches the ground
+        if (grounded)
+            doubleJumped = false;
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        //Enables Jump
+        if (Input.GetKeyDown(KeyCode.W) && grounded)
         {
             GetComponent<Rigidbody2D>().velocity = new Vector2(0, jumpHeight);
         }
 
-        if (Input.GetKeyDown(KeyCode.D))
+        //Allows Double Jumping
+        if (Input.GetKeyDown(KeyCode.W) && !doubleJumped && !grounded)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, jumpHeight);
+            doubleJumped = true;
+        }
+
+        //Controls Player's horizontal forward movement
+        if (Input.GetKey (KeyCode.D))
         {
             GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, 0);
         }
 
-        if (Input.GetKeyDown(KeyCode.A))
+        //Controls Player's horizontal backward movement
+        if (Input.GetKey (KeyCode.A))
         {
             GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, 0);
         }
