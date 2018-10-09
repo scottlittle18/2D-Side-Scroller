85461d37d03a4df8b79a9efac858099636ff0f6e
Scott Little <scott.little@loop.colum.edu>
2018-10-09 10:52:06 -0500

    Add enemyPatrol script
    - adding enemy patrol script
         - working on the wallCheck event

---
+++ Assets/Scripts/EnemyPatrol.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+
+    [SerializeField]
+    private float moveSpeed, wallCheckRadius;
+
+    private bool hittingWall, moveRight;
+
+    [SerializeField]
+    private Transform wallCheck;
+    [SerializeField]
+    private LayerMask whatIsWall;
+
+    [SerializeField]
+    private Rigidbody2D enemyRigidBody;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        Patrol();
+
+        hittingWall = Physics2D.OverlapCircle(wallCheck.position, 
+            wallCheckRadius, whatIsWall);
+
+        
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    public void Patrol()
+    {
+        if (hittingWall)
+            moveRight = !moveRight;
+
+        if (moveRight)
+            enemyRigidBody.velocity = new Vector2(moveSpeed, enemyRigidBody.velocity.y);
+        else
+            enemyRigidBody.velocity = new Vector2(-moveSpeed, enemyRigidBody.velocity.y);
+    }
+}
