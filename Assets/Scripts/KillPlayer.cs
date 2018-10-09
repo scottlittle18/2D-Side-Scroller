12b76d3d2d2c4834aea1311f587ed46cab36f30a
Scott Little <scott.little@loop.colum.edu>
2018-10-09 17:33:18 -0500

    Update to KillPlayer Script
    - Update was made to the KillPlayer script.
         - update to script includes removing the GetComponent<>, Start(), and Update() functions to make the script more streamline

--- Assets/Scripts/KillPlayer.cs
+++ Assets/Scripts/KillPlayer.cs
@@ -7,19 +7,8 @@ public class KillPlayer : MonoBehaviour
     [SerializeField]
     private LevelManager levelManager;
 
-    // Use this for initialization
-    void Start()
-    {
-        levelManager = FindObjectOfType<LevelManager>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {        
-
-    }
-
-    
+    //Start() and Update() are not needed for this script 
+    //  it is looking for a collision and doesnt need to update
     public void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
