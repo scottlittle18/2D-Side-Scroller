72ab340963e00dd173a35a604b8be41246193599
Scott Little <scott.little@loop.colum.edu>
2018-10-16 11:25:27 -0500

    Add Bandit Run Anim
    - Need to edit the script
    - Need to rescale the PNG for the bandit's Idle Anim

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Door : MonoBehaviour
+{
+    
+    BoxCollider2D box;
+
+    int currentScene;
+
+    private void Start()
+    {
+        currentScene = SceneManager.GetActiveScene().buildIndex;
+    }
+
+    private void Awake()
+    {
+        box = GetComponent<BoxCollider2D>();
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        Debug.Log("Collision detected.");
+
+        if ((Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W)) && other.tag == "Player")
+        {
+            Debug.Log("Up command registered.");
+
+            SceneManager.LoadScene(currentScene + 1);
+        }
+    }
+}
