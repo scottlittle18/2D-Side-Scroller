04d20ee9637611918840338f6f263ba91791a747
Scott Little <scott.little@loop.colum.edu>
2018-10-23 09:39:19 -0500

    Add Main Menu
    - Added a rough Main Menu that will be polished later.

---
+++ Assets/Scripts/MenuButtons.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtons : MonoBehaviour {
+
+    int currentScene;
+
+	// Use this for initialization
+	void Start () {
+        currentScene = SceneManager.GetActiveScene().buildIndex;
+	}
+	
+    public void PressStartButton()
+    {
+        SceneManager.LoadScene(currentScene + 1);
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void ExitCredits()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
